fcb5d036ee8bfaceb519cdf3d7863090d174ee03
miss-gif <svx327@naver.com>
2024-04-16 13:20:29 +0900

    Docs : 함수컴포넌트이벤트

--- src/Main.js
+++ src/Main.js
@@ -1,45 +1,20 @@
 import { useState } from "react";
 
-const Main = ({ title, children }) => {
-  const [message, setMessage] = useState("");
-
-  const [color, setColor] = useState("black");
-
-  const onClickEnter = () => {
-    setMessage("안녕");
-    setColor("red");
-  };
-
-  const onClickLeave = () => {
-    setMessage("바이");
-    setColor("blue");
-  };
-
-  const array = [
-    { id: 1, value: true },
-    { id: 2, value: true },
-    { id: 3, value: false },
-    { id: 4, value: true },
-    { id: 5, value: false },
-    { id: 6, value: true },
-  ];
-
-  let nextArray = array.concat({ id: 7 }); // 새 항목 추가
-  nextArray = nextArray.filter(item => item.id !== 2); // id가 2인 항목 제거
-  // id가 1인 항목의 value를 false로 설정
-  nextArray = nextArray.map(item =>
-    item.id === 1 ? { ...item, value: false } : item,
-  );
-
-  console.log(array);
-  console.log(nextArray);
+const Main = () => {
+  const [username, setUsername] = useState("");
+  const onChangeUsername = e => setUsername(e.target.value);
 
   return (
-    <>
-      <button onClick={onClickEnter}>입장</button>
-      <button onClick={onClickLeave}>퇴장</button>
-      <p style={{ color }}>{message}</p>
-    </>
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+    </div>
   );
 };
 
