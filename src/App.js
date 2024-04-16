173a1737607366c4896de20b52939b29270a13a1
miss-gif <svx327@naver.com>
2024-04-16 10:45:01 +0900

    Docs : 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Main from "./Main";
 
 function App() {
-  return <Main title="리액트" />;
+  return <Main title="App에서 온 title값">Main의 칠드런</Main>;
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Main = props => {
+const Main = ({ title, children }) => {
+  // const { title, children } = props;
+
   return (
     <div>
-      <h1>안녕하세요, 나는{props.title}입니다.</h1>
+      <h1>title, 나는{title}입니다.</h1>
+      <h1>children, 나는{children}입니다.</h1>
     </div>
   );
+};
+
+Main.defaultProps = {
+  title: "기본 이름",
 };
 
 export default Main;
