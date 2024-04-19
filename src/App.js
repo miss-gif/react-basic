446ed470212243e7a71b9fe867c70cfc25cd738c
miss-gif <svx327@naver.com>
2024-04-19 13:19:17 +0900

    Dosc : 리액터 라우터

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import MainPage from "./pages/MainPage";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
-const App = () => {
+function App() {
   return (
-    <>
-      <MainPage />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Routes>
+    </div>
   );
-};
+}
 
 export default App;
