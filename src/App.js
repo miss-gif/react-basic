8d841f9916b471823cd4f8866716c0df0ec7446c
miss-gif <svx327@naver.com>
2024-04-22 13:18:23 +0900

    Docs : 리액트 라우터

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
+import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/:username" element={<Profile />} />
-      </Routes>
-    </div>
+      </Route>
+
+      <Route path="/articles" element={<Articles />}>
+        <Route index element={<Navigate replace to="1" />} />
+        <Route path=":id" element={<Article />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
